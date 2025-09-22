97c8bc46d523d03d2d903b8ff5cb159496cf0e67
SHEHARYAR <mohmmadsheheryar006@gmail.com>
2025-09-22 22:27:47 +0500

    Sheharyar

---
+++ Sheharyar/task1_variables.py
@@ -0,0 +1,11 @@
+#all inputs are string by default
+
+name = "Sheharyar"
+age = 19
+height = 5.8
+is_student = True
+
+print("My name is", name)
+print("My age is", age)
+print("My height is", height)
+print("I am a student", is_student)
---
+++ Sheharyar/task2_operations.py
@@ -0,0 +1,10 @@
+a = 5
+b = 4
+
+print("Add:", a+b)
+print("subtract:", a-b)
+print("multiplication:", a*b)
+print("division:", a/b)
+print("division without decimals:", a//b)
+print("remainder:", a%b)
+print("a(5) raised to power b(4):", a**b)
